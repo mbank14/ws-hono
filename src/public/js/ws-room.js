b98e01e39a22f32ef86d7ae8ace5166f72c305dd
mbank14 <karangvisual@pm.me>
2025-07-30 10:13:18 +0700

    add conditional message user

--- src/public/js/ws-room.js
+++ src/public/js/ws-room.js
@@ -1,18 +1,56 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const ws = new WebSocket("ws://localhost:3000/ws/play");
-    
+    const descReadyUser = document.getElementById("descReadyUser");
+
+    let data ;
+    let parsedMessage = "";
+
     ws.onopen = (evt) => {
         console.log("Connect ws");
     }
-    
+
     ws.onmessage = (evt) => {
-        let parsedMessage = JSON.parse(evt.data);
+        parsedMessage = JSON.parse(evt.data);
 
         console.log(parsedMessage);
     }
 
-    document.getElementById("playBtn").addEventListener("click", ()=>{
-        ws.send(JSON.stringify({message: "Klik ready"}))
-    })
+    document.getElementById("playBtn").addEventListener("click", () => {
+        const playBtn = document.getElementById("playBtn");
+        const data = parsedMessage ;
+        if (!data) return console.log("Data dari server notfound");
 
-})
+        if (data.isReady == true) {
+            ws.send(JSON.stringify({
+                type: "STATUS_USER",
+                isReady: false
+            }));
+            playBtn.classList.remove("!bg-rose-600");
+            playBtn.textContent = "Play"
+        };
+
+        ws.send(JSON.stringify({
+            type: "STATUS_USER",
+            isReady: true
+        }));
+        playBtn.classList.add("!bg-rose-600");
+        playBtn.textContent = "Cancel"
+    });
+
+
+    descReadyUser.innerHTML = '';
+    let desc
+    if(data){
+        desc = document.createElement("p")
+        desc.textContent = `tinggal ${2} orang lagi, colek temen kamu !`
+    }else{
+        desc = document.createElement("p")
+        desc.textContent = `Belum ada yang siap, jadilah yang pertama !`
+    }
+
+    descReadyUser.appendChild(desc);
+
+
+});
--- src/views/Room.tsx
+++ src/views/Room.tsx
@@ -10,15 +10,7 @@ export const RoomPage = () =>{
     return(
         <Layout text="Play Room">
             <div>
-                <div>
-
-                {data ? (
-                    <p>
-                        tinggal 2 orang lagi yang belum, colek temen kamu sekarang !
-                    </p>
-                ) : (
-                    <p>Belum ada yang mulai, jadilah yang pertama dan ingatkan teman kamu!!</p>
-                )}
+                <div id="descReadyUser">
                 </div>
 
                 <div>
